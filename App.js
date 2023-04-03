f4cffc80c7258eb536e40d15a65b86af8fa266e2
lonaxit <lonaxit@outlook.com>
2023-04-03 11:16:33 +0100

    Updated files
    Updated files

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+//import * as Font from 'expo-font';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import HomeStack from './navigation/HomeStack';
+import AuthStack from './navigation/AuthStack';
+
+
+
 
 export default function App() {
+
+    const [fontsLoaded] = useFonts({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+    'nunito-medium': require('./assets/fonts/Nunito-Medium.ttf'),
+    'nunito-light': require('./assets/fonts/Nunito-Light.ttf'),
+    'nunito-extralight': require('./assets/fonts/Nunito-ExtraLight.ttf'),
+    'nunito-boldItalic': require('./assets/fonts/Nunito-BoldItalic.ttf'),
+    'nunito-mediumItalic': require('./assets/fonts/Nunito-MediumItalic.ttf'),
+    });
+  
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync()
+    }
+    prepare();
+  }, [])
+  
+  if (!fontsLoaded) {
+    return undefined
+  } else {
+    SplashScreen.hideAsync()
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  
+    <NavigationContainer>
+    <StatusBar backgroundColor="#ffffff"/>
+    {/* <HomeStack/> */}
+    <AuthStack/>
+   </NavigationContainer>
+   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
