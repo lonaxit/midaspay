a4fdb800df0208950b4ba264fb4e997b10f366b6
lonaxit <lonaxit@outlook.com>
2023-04-05 07:28:01 +0100

    Update welcomeHeader.js
    Changed menu image

--- components/common/headers/welcome/welcomeHeader.js
+++ components/common/headers/welcome/welcomeHeader.js
@@ -2,19 +2,22 @@ import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { ImageBackground, TouchableOpacity } from 'react-native'
 import { Text, View } from 'react-native'
+import { useAuth } from '../../../../Context/AuthContext'
 import styles from './welcomeheader.style'
 
+useAuth
 
 
 const WelcomeHeader = () => {
-    const navigation = useNavigation()
+  const navigation = useNavigation()
+
   return (
     <View style={styles.welcomeHeader}>
      
-          <Text>Hello Luper</Text>
+          <Text>Hello</Text>
           <TouchableOpacity onPress={()=>navigation.openDrawer()}>
           <ImageBackground
-            source={require('../../../../assets/images/bye.png')}
+            source={require('../../../../assets/images/menu.png')}
             style={{ width: 35, height: 35 }}
             imageStyle={{borderRadius:25}}
                   />
