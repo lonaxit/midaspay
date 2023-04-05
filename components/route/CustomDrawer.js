eb2cceba2e1a3a7232ee20b5515b0bf3c0b82959
lonaxit <lonaxit@outlook.com>
2023-04-05 07:29:34 +0100

    Update CustomDrawer.js
    Changed custom drawer background color

--- components/route/CustomDrawer.js
+++ components/route/CustomDrawer.js
@@ -2,10 +2,15 @@ import React from 'react'
 import { View, Text,ImageBackground,Image,TouchableOpacity } from 'react-native'
 import {DrawerContentScrollView, DrawerItemList} from '@react-navigation/drawer'
 import { Ionicons } from '@expo/vector-icons';
+import { useAuth } from '../../Context/AuthContext';
+
+
 const CustomDrawer = (props) => {
+// const { logout } = useAuth()
+    
     return (
       <View style={{flex:1}}>
-    <DrawerContentScrollView {...props} contentContainerStyle={{backgroundColor:'#c70'}}>
+    <DrawerContentScrollView {...props} contentContainerStyle={{backgroundColor:'#fff'}}>
             <ImageBackground source={require('../../assets/images/logo.png')} style={{ padding: 20 }}>
                 <Image  source={require('../../assets/images/user.png')} style={{height:80, width:80, borderRadius:40, marginBottom:10}}/> 
                     <Text style={{ fontSize: 20 }}>Tivkaa Luper</Text> 
@@ -30,7 +35,7 @@ const CustomDrawer = (props) => {
                     </View>
                 </TouchableOpacity>
                 
-                <TouchableOpacity style={{ paddingVertical: 15 }} onPress={() => { }}>
+                <TouchableOpacity style={{ paddingVertical: 15 }} onPress={() => { logout()} }>
                     <View style={{flexDirection:'row',alignItems:'center'}}>
                     <Ionicons name="exit-outline" size={20} color="black" />
                     <Text style={{fontSize:15,marginLeft:5}}>Sign Out </Text>
