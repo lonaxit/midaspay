fdbd65822c6d91f7c985f0365c8996fed6f87b96
lonaxit <lonaxit@outlook.com>
2023-04-06 08:15:02 +0100

    Create LoanDetail.js
    new file

---
+++ screens/LoanDetail.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LoanDetail = () => {
+  return (
+    <View>
+      <Text>LoanDetail</Text>
+    </View>
+  )
+}
+
+export default LoanDetail
