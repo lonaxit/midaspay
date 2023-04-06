a8148c850e6b8c9fde3acd80eb919ba7afef1904
lonaxit <lonaxit@outlook.com>
2023-04-06 16:46:57 +0100

    Update SavingBalCard.js
    Created code to use userData state

--- components/savings/savingCards/SavingBalCard.js
+++ components/savings/savingCards/SavingBalCard.js
@@ -1,20 +1,46 @@
 
 import { View, Text , TouchableOpacity, Image} from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import styles from './savingbal.style'
+import { BASE_URL_AUTH } from '../../../config'
+import axios from 'axios'
+import { useAuthentication } from '../../../Context/Authentication'
+import { ActivityIndicator } from 'react-native';
+
+ 
+
+const SavingBalCard = ({route}) => {
 
 
+  const { authtoken,userData,authenticated, fetchUserData }= useAuthentication()
 
+  useEffect(() => {
 
-const SavingBalCard = () => {
-  const [balance, setBalance] = useState(2001258)
+    // because fetchUserData returns a promise, we can wrap in an async/wait
+    //when fetch data returns a value
+    // async function getuserData() {
+    //   await fetchUserData();
+    // }
+    // getuserData()
+    fetchUserData();
+
+  }, []);
+
+  if (!userData) {
+    return (
+      <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+        <ActivityIndicator size={'large'} />
+    </View>
+    )
+  }
+
   return (
 <TouchableOpacity>
-            <View style={styles.card}>
-                            <Text style={styles.balance}>₦{balance.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Text>
-                            <Text style={styles.label}>Available on your savings</Text>
-            </View>
+        <View style={styles.card}>
+              <Text style={styles.balance}>₦{userData.totalSaving.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Text>
+              <Text style={styles.label}>Available on your savings</Text>
+        </View>
   </TouchableOpacity>
   )
 }
