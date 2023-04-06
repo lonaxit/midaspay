6525c51d367caf105b6ce6d0bd58438591546a1f
lonaxit <lonaxit@outlook.com>
2023-04-06 08:11:18 +0100

    Update CustomDrawer.js
    Removed background image

--- components/route/CustomDrawer.js
+++ components/route/CustomDrawer.js
@@ -2,20 +2,37 @@ import React from 'react'
 import { View, Text,ImageBackground,Image,TouchableOpacity } from 'react-native'
 import {DrawerContentScrollView, DrawerItemList} from '@react-navigation/drawer'
 import { Ionicons } from '@expo/vector-icons';
-import { useAuth } from '../../Context/AuthContext';
+import { useAuthentication } from '../../Context/Authentication';
+// import { useAuth } from '../../Context/AuthContext';
+
 
 const CustomDrawer = (props) => {
 // const { logout } = useAuth()
+
+const { logout, authenticated,authtoken } = useAuthentication()
     
     return (
       <View style={{flex:1}}>
     <DrawerContentScrollView {...props} contentContainerStyle={{backgroundColor:'#fff'}}>
-            <ImageBackground source={require('../../assets/images/logo.png')} style={{ padding: 20 }}>
+            {/* <ImageBackground source={require('../../assets/images/logo.png')} style={{ padding: 20 }}>
                 <Image  source={require('../../assets/images/user.png')} style={{height:80, width:80, borderRadius:40, marginBottom:10}}/> 
                     <Text style={{ fontSize: 20 }}>Tivkaa Luper</Text> 
                     <Text style={{ fontSize: 16 }}>O-08</Text>  
                     <Text style={{fontSize:12}}>317752</Text>     
-          </ImageBackground>
+            </ImageBackground> */}
+
+
+             <View style={{padding:20}}>
+             <Image  source={require('../../assets/images/user.png')} style={{height:80, width:80, borderRadius:40, marginBottom:10}}/> 
+                    <Text style={{ fontSize: 20 }}>Tivkaa Luper</Text> 
+                    <Text style={{ fontSize: 16 }}>O-08</Text>  
+                    <Text style={{ fontSize: 12 }}>317752</Text>
+                 
+                    
+             </View>
+               
+                
+
 
             <View style={{flex:1,backgroundColor:'#fff',paddingTop:10}}>
             <DrawerItemList {...props} />   
