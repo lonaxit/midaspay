9965e48340ce982801d8aa6b414753733cc060c6
lonaxit <lonaxit@outlook.com>
2023-05-03 08:42:32 +0100

    Converted string to float

--- components/OverviewWelcomeHeader.js
+++ components/OverviewWelcomeHeader.js
@@ -2,11 +2,19 @@ import { View, Text, Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 import { AntDesign } from '@expo/vector-icons';
 import { ImageBackground } from 'react-native'
+import { useAuth } from '../contextAPI/AuthContext'
+import { ActivityIndicator } from 'react-native';
 
-const OverviewWelcomeHeader = ({onPress}) => {
+
+const OverviewWelcomeHeader = ({ onPress }) => {
+
+    const { userData } = useAuth()
+
+
+    
   return (
 <View style={styles.welcomeHeader}>
-    <Text>Hello</Text>
+    <Text>Hello {userData.last_name}</Text>
           <Pressable  onPress={onPress}>
           <AntDesign name="lock1" size={24} color="black" />
     {/* <ImageBackground
@@ -25,7 +33,8 @@ const styles = StyleSheet.create({
     welcomeHeader: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginBottom:20
+        marginBottom: 20,
+        marginTop:40
     },
     welcomeText: {
         fontSize: 16,
