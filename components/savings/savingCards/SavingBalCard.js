8f64e5a755f7fda937611428fbb6303d775a51fc
lonaxit <lonaxit@outlook.com>
2023-05-04 13:33:18 +0100

    Update SavingBalCard.js
    Removed code

--- components/savings/savingCards/SavingBalCard.js
+++ components/savings/savingCards/SavingBalCard.js
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react'
 import styles from './savingbal.style'
 import { BASE_URL_AUTH } from '../../../config'
 import axios from 'axios'
-import { useAuthentication } from '../../../Context/Authentication'
+
 import { ActivityIndicator } from 'react-native';
 
  
@@ -13,27 +13,17 @@ import { ActivityIndicator } from 'react-native';
 const SavingBalCard = ({route}) => {
 
 
-  const { authtoken,userData,authenticated, fetchUserData }= useAuthentication()
 
-  useEffect(() => {
 
-    // because fetchUserData returns a promise, we can wrap in an async/wait
-    //when fetch data returns a value
-    // async function getuserData() {
-    //   await fetchUserData();
-    // }
-    // getuserData()
-    fetchUserData();
 
-  }, []);
 
-  if (!userData) {
-    return (
-      <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-        <ActivityIndicator size={'large'} />
-    </View>
-    )
-  }
+  // if (!userData) {
+  //   return (
+  //     <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+  //       <ActivityIndicator size={'large'} />
+  //   </View>
+  //   )
+  // }
 
   return (
 <TouchableOpacity>
