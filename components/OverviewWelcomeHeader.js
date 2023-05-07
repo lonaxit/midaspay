a669e7115c2f1a9c927674d38af20fcdddbd0ea5
lonaxit <lonaxit@outlook.com>
2023-05-07 22:37:18 +0100

    Update OverviewWelcomeHeader.js
    Updated header with user lastname

--- components/OverviewWelcomeHeader.js
+++ components/OverviewWelcomeHeader.js
@@ -2,11 +2,13 @@ import { View, Text, Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 import { AntDesign } from '@expo/vector-icons';
 import { ImageBackground } from 'react-native'
+import { useMidasAuth } from '../AppStore/AuthorizationContext'
 
-const OverviewWelcomeHeader = ({onPress}) => {
+const OverviewWelcomeHeader = ({ onPress }) => {
+const {userInfo} = useMidasAuth()
   return (
 <View style={styles.welcomeHeader}>
-    <Text>Hello</Text>
+    <Text style={{fontWeight:'bold'}}>Hello {userInfo.last_name}</Text>
           <Pressable  onPress={onPress}>
           <AntDesign name="lock1" size={24} color="black" />
     {/* <ImageBackground
