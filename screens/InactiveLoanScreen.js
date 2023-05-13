88692c88638fc38a4753d9460ec55673bbe69b7b
lonaxit <lonaxit@outlook.com>
2023-05-13 09:56:13 +0100

    Update InactiveLoanScreen.js
    Added code for inactive loans

--- screens/InactiveLoanScreen.js
+++ screens/InactiveLoanScreen.js
@@ -1,9 +1,16 @@
-import { useNavigation, useRoute } from '@react-navigation/native'
-import React from 'react'
-import { View, Text } from 'react-native'
- useRoute
 
-const InactiveLoanScreen = ({route,navigation}) => {
+import React from 'react'
+import { View, Text, StyleSheet,FlatList,ActivityIndicator } from 'react-native'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { useMidasAuth } from '../AppStore/AuthorizationContext'
+import Item from '../components/Item'
+
+const InactiveLoanScreen = ({ route, navigation }) => {
+  const { userInfo } = useMidasAuth()
+
+  function handleActiveLoanNavigation(id) {
+    navigation.navigate('activeloandetail',{loanId:id})
+}
 
   // const navigation = useNavigation()
   //  const route = useRoute()
@@ -12,12 +19,68 @@ const InactiveLoanScreen = ({route,navigation}) => {
   // const { id } = route.params
   // changing title dynamically
   // navigation.setOptions({ title: id });
-  return (
-      <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      {/* <Text>Inactive Loan Screen {route.params?.id}</Text> */}
-      <Text>Inactive Loan Screen </Text>
+  let inactiveLoans = userInfo.loanowner.filter(item => item.active ===false)
+
+  if (!userInfo.loanowner) {
+    return (
+      <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+        <ActivityIndicator size={'large'} />
     </View>
+    )
+}
+  return (
+    <View style={styles.rootContainer}>
+    <View>
+      <Text style={styles.title}>Paid Loans</Text>
+      <View>
+          <Text style={styles.totalTitle}>{inactiveLoans.length}</Text>
+      </View>
+
+     
+      
+    </View>
+    
+    <FlatList
+               data={inactiveLoans}
+                renderItem={({ item }) => (
+                    <Item
+                        title={item.product_name}
+                        loan_date={item.start_date}
+                        deduction={item.monthly_deduction}
+                        totalamount={item.approved_amount}
+                        onPress={()=>handleActiveLoanNavigation(item.id)}
+                    />  
+                 )}
+                 keyExtractor={(item) => item.id.toString()}
+                 showsVerticalScrollIndicator={false}
+          />        
+  </View>
   )
 }
 
 export default InactiveLoanScreen
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    paddingTop:30,
+    paddingRight: 20,
+    paddingLeft: 20,
+    backgroundColor:'#fff'
+  },
+  title:{
+    // marginBottom: 5,
+    textAlign: 'center',
+    fontSize: 32,
+    fontWeight:'bold'
+  },
+  totalTitle: {
+  
+    color: '#95a5a6',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 4,
+    textAlign:'center'
+  },
+
+})
