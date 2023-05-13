c143f85422717599ef039e63d156db98394c5d5f
lonaxit <lonaxit@outlook.com>
2023-05-13 09:56:38 +0100

    Update LoanHomeScreen.js
    Added code for loan home screen

--- screens/LoanHomeScreen.js
+++ screens/LoanHomeScreen.js
@@ -1,12 +1,77 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet,FlatList, ActivityIndicator } from 'react-native'
+import { useMidasAuth } from '../AppStore/AuthorizationContext'
+import Item from '../components/Item'
 
-const LoanHomeScreen = () => {
+const LoanHomeScreen = ({navigation}) => {
+
+  const { userInfo } = useMidasAuth()
+
+  function handleActiveLoanNavigation(id) {
+    navigation.navigate('activeloandetail',{loanId:id})
+}
+
+  if (!userInfo.loanowner) {
+    return (
+      <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+        <ActivityIndicator size={'large'} />
+    </View>
+    )
+}
+
+
   return (
-      <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-          <Text>Loan Home Screen </Text>
+    <View style={styles.rootContainer}>
+      <View>
+        <Text style={styles.title}>All Loans</Text>
+        <View>
+            <Text style={styles.totalTitle}>{userInfo.loanowner.length}</Text>
+        </View>
+
+       
+        
+      </View>
+      
+      <FlatList
+                 data={userInfo.loanowner}
+                  renderItem={({ item }) => (
+                      <Item
+                          title={item.product_name}
+                          loan_date={item.start_date}
+                          deduction={item.monthly_deduction}
+                          totalamount={item.approved_amount}
+                          onPress={()=>handleActiveLoanNavigation(item.id)}
+                      />  
+                   )}
+                   keyExtractor={(item) => item.id.toString()}
+                   showsVerticalScrollIndicator={false}
+            />        
     </View>
   )
 }
 
 export default LoanHomeScreen
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    paddingTop:30,
+    paddingRight: 20,
+    paddingLeft: 20,
+    backgroundColor:'#fff'
+  },
+  title:{
+    // marginBottom: 5,
+    textAlign: 'center',
+    fontSize: 32,
+    fontWeight:'bold'
+  },
+  totalTitle: {
+    color: '#95a5a6',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 4,
+    textAlign:'center'
+  },
+
+})
