4581acc9b3acb46398c5119fb1147c0395d25916
lonaxit <lonaxit@outlook.com>
2023-05-30 17:23:42 +0100

    Update LoanSummary.js
    Refactored loan summary

--- components/LoanSummary.js
+++ components/LoanSummary.js
@@ -1,14 +1,28 @@
-import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { View, Text, Pressable, StyleSheet,TouchableOpacity } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient';
 import React, { useState } from 'react'
+import { useMidasAuth } from '../AppStore/AuthorizationContext'
+import { useNavigation } from '@react-navigation/native'
 
+const LoanSummary = ({ amount, topTitle, bottomTitle }) => {
+  const nav =  useNavigation()
+  const { logout, userInfo, fetchUser } = useMidasAuth()
 
-const LoanSummary = ({amount,topTitle,bottomTitle}) => {
+  if (!userInfo.loanowner) {
+    return (
+      <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+        <ActivityIndicator size={'large'} />
+    </View>
+    )
+}
+
+  let activeLoans = userInfo.loanowner.filter(loan => loan.active === true)
+  let inactiveLoans = userInfo.loanowner.filter(loan => loan.active === false)
  
   return (
     <Pressable >
     <LinearGradient
-      colors={['#CD5C5C', '#CD5C5C']}
+      colors={['#C96D3C', '#C96D3C']}
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
       style={styles.loanContainer}
@@ -22,10 +36,23 @@ const LoanSummary = ({amount,topTitle,bottomTitle}) => {
             <Text style={styles.balanceText}>₦{parseFloat(amount).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Text>
         </View>
 
-        <View>
+        {/* <View>
           <Text style={styles.lightText}>{bottomTitle}</Text>
       
-            </View>
+        </View> */}
+
+        <View style={styles.analysisContainer}>
+          <Text style={styles.lightText}>Active</Text>
+          <Text style={styles.lightText}>Inactive</Text>
+        </View>
+        <View style={styles.analysisContainer}>
+          <Text>{activeLoans.length}</Text>
+          <TouchableOpacity style={styles.welcomeBtn} onPress={()=>nav.navigate('inactiveloans')}>
+            {/* <Text style={styles.welcomeBtnText}>Let's Begin</Text> */}
+            <Text style={styles.inactiveText}>{inactiveLoans.length}</Text>
+          </TouchableOpacity>
+       
+        </View>
             
       </LinearGradient>
 
@@ -40,7 +67,7 @@ const styles = StyleSheet.create({
     loanContainer: {
         borderRadius: 8,
         padding: 16,
-        alignItems: 'center',
+        // alignItems: 'center',
         justifyContent: 'center',
         marginTop: 50,
         minHeight: 120,
@@ -51,17 +78,38 @@ const styles = StyleSheet.create({
         fontSize: 32,
         fontWeight: 'bold',
         textAlign: 'center',
+        marginBottom:11
       },
     boldText: {
           fontSize:12,
           fontWeight: 'bold',
           textAlign: 'center',
           color: '#fff',
-      },
+  },
+  inactiveText: {
+    fontSize:14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#00221f',
+  },
     lightText: {
           fontSize:14,
           textAlign: 'center',
-          fontWeight: '600',
+          fontWeight: '700',
           color: '#fff',
     },
+
+  analysisContainer: {
+    flexDirection: 'row',
+    justifyContent:'space-between'
+  },
+  welcomeBtn: {
+    backgroundColor: '#ff8442',
+    padding: 5,
+    width: '17%',
+    borderRadius: 5,
+    // flexDirection: 'row',
+    //   justifyContent: 'space-between',
+    // marginBottom: 50
+  }
 })
