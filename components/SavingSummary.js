a92064f0941d3c0d77f7a8c0a36450f1f0a1c924
lonaxit <lonaxit@outlook.com>
2023-05-30 17:24:10 +0100

    Update SavingSummary.js
    Added propos

--- components/SavingSummary.js
+++ components/SavingSummary.js
@@ -6,7 +6,7 @@ import { useNavigation } from '@react-navigation/native'
 
 
 
-const SavingSummary = ({ totalsaving }) => {
+const SavingSummary = ({ title,totalsaving, loanBal }) => {
   
   const nav =  useNavigation()
 const [balance,setBalance] = useState(142587)
@@ -21,12 +21,29 @@ const [balance,setBalance] = useState(142587)
       }
     
   return (
-<Pressable android_ripple={{color:'#ccc'}} onPress={()=>nav.navigate('usersavingdetail',{depositTotal:totalsaving})}>
-        <View style={styles.card}>
+
+    <View style={styles.card}>
+
+    <Pressable onPress={()=>nav.navigate('usersavingdetail',{depositTotal:totalsaving})}>
+        <View >
+         <Text style={styles.savingText}>SAVINGS</Text>
               <Text style={styles.balance}>₦{parseFloat(totalsaving).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Text>
-              <Text style={styles.label}>Available on your savings</Text>
+              
         </View>
-  </Pressable>
+      </Pressable>
+      
+      {/* <View style={styles.divider} /> */}
+
+      <View style={styles.loanBalContainer}>
+        <Text style={styles.loanBalText}>{title} </Text>
+        { loanBal &&    <Pressable  onPress={() => nav.navigate('Loans')}>
+        <Text style={styles.loanBal}>₦{parseFloat(loanBal).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</Text>
+        </Pressable>
+        } 
+        </View>
+
+    </View>
+
   )
 }
 
@@ -37,27 +54,57 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         borderRadius: 8,
         padding: 16,
-        height:100,
+        // height:100,
         // styles for ios
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.1,
         shadowRadius: 4,
         // for android
-        elevation: 4,
+        elevation: 20,
       
-      },
+  },
+  
       balance: {
-        fontSize: 24,
+        fontSize: 32,
         fontWeight: 'bold',
-        textAlign: 'center',
+        // textAlign: 'center',
           marginBottom: 8,
-        fontFamily:'nunito-bold'
+        fontFamily: 'nunito-bold',
+        // color:'#1D5330'
+        color:'#239b56'
       },
-      label: {
-        fontSize: 13,
-        color: '#9B9B9B',
-          textAlign: 'center',
+      savingText: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: '#239b56',
+        // textAlign: 'center',
         fontFamily:'nunito-medium'
-    },
+  },
+      
+    divider: {
+      height: 1,
+      width: '100%',
+      backgroundColor: 'black',
+      marginBottom:10
+  },
+  loanBalContainer: {
+    flexDirection: 'row'
+  },
+
+  loanBalText: {
+    fontSize: 14,
+    color: 'black',
+    fontWeight:'bold',
+    fontFamily: 'nunito-medium',
+    marginRight:10
+  },
+
+  loanBal: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    fontFamily: 'nunito-medium',
+    color:'red'
+  }
+  
 });
